5cce04e726172648e225700b9bf11ac7188c764f
熊宇然 <1914999099@qq.com>
2019-11-23 14:59:13 +0800

    写了个地皮的类；风力倾斜度啥的还没想好怎么写

---
+++ FarmAndGolfProject/Assets/Scripts/Ground.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Ground
+{
+    protected string groundType;  //地皮的类型，例如“lawn”、“snowfield”
+    protected float groundFrictionFactor;  //不同地皮摩擦力影响因子不同
+    protected float groundJumpFactor;  //不同地皮弹跳影响因子不同
+    private Sprite groundSp;  //地皮贴图
+    protected Collider cd;  //碰撞器
+    
+    public float GroundJumpFactor  //地皮弹跳因子
+    { get { return groundJumpFactor; } }
+    public float GroundFrictionFactor  //地皮摩擦力因子
+    { get { return groundFrictionFactor; } }
+    public string GroundType  //地皮类型
+    { get { return groundType; } }
+    public Sprite GroundSprite  //地皮贴图
+    {
+        set { groundSp = value; }
+    }
+    public Collider collider  //碰撞器
+    {
+        set { cd = value; }
+        get { return cd; }
+    }
+    //地面与球接触后的特殊效果，如球入洞，球入水后的波纹，球在雪地上砸出坑，滚出痕迹等
+    public abstract void GroundEffect();
+
+
+}
