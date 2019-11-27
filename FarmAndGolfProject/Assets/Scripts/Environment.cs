43db1682f500acfe59d0b1e993661ec3e5fce696
熊宇然 <1914999099@qq.com>
2019-11-27 23:01:08 +0800

    Revert "学着学姐写了完善了俩抽象类…"
    This reverts commit d30f5816244eb206dfaf771de653ab608a9cd5a2.

--- FarmAndGolfProject/Assets/Scripts/Environment.cs
+++ FarmAndGolfProject/Assets/Scripts/Environment.cs
@@ -4,20 +4,5 @@ using UnityEngine;
 
 public abstract class Environment
 {
-    protected float windPower;  //环境风力
-    protected Vector3 windPowerDirection;  //环境风向
-    private Sprite windSp;  //风向标图标
-    protected float windSpAngle;  //风向标旋转角度
 
-    public float WindPower  //环境风力
-    { get { return windPower; } }
-    public Vector3 WindPowerDirection  //环境风向
-    { get { return windPowerDirection; } }
-    public Sprite WindSprite  //风向标图标
-    {
-        set { windSp = value; }
-    }
-    public float WindSpriteAngle  //风向标旋转角度
-    { get { return windSpAngle; } }
-    public abstract void EnvironmentEffect();  //环境特效，如飘雪特效，蒲公英漂浮特效
 }
--- FarmAndGolfProject/Assets/Scripts/Floor.cs
+++ FarmAndGolfProject/Assets/Scripts/Floor.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public abstract class Floor
 {
-    protected string groundType;  //地皮的类型，如草地，雪地
-    protected float groundFrictionFactor;  //不同地皮摩擦力因数不同
+    protected string groundType;  //地皮的类型，例如“lawn”、“snowfield”
+    protected float groundFrictionFactor;  //不同地皮摩擦力影响因子不同
     protected float groundBounceFactor;  //不同地皮弹力影响因子不同
     private Sprite groundSp;  //地皮贴图
     protected Collider cd;  //碰撞器
 
     public float GroundBounceFactor  //地皮弹力因子
     { get { return groundBounceFactor; } }
-    public float GroundFrictionFactor  //地皮摩擦力因数
+    public float GroundFrictionFactor  //地皮摩擦力因子
     { get { return groundFrictionFactor; } }
     public string GroundType  //地皮类型
     { get { return groundType; } }
@@ -25,6 +25,6 @@ public abstract class Floor
         set { cd = value; }
         get { return cd; }
     }
-    public abstract void CollisionEffect();  //碰撞效果，如球入洞，球入水后的波纹，球在雪地上砸出坑，滚出痕迹等
-    public abstract void GroundEffect();  //地皮特效，如水波荡漾，草丛摇摆
+    //地面与球接触后的特殊效果，如球入洞，球入水后的波纹，球在雪地上砸出坑，滚出痕迹等
+    public abstract void GroundEffect();
 }
