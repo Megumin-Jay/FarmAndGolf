7b3a39971b1c7b16e1e47e68eb291c1ec6e3ff46
texwood0935 <1213712563@qq.com>
2019-11-27 17:43:43 +0800

    修改了一下球的类，添加了一些属性，修改了一些属性

--- FarmAndGolfProject/Assets/Scripts/Ball.cs
+++ FarmAndGolfProject/Assets/Scripts/Ball.cs
@@ -4,33 +4,37 @@ using UnityEngine;
 
 public abstract class Ball
 {
-    protected float quality;  //球的质量
-    protected string type;    //球的类型，例如："apple"、"orange"之类的
-    protected float jumpfactor;   //弹跳的影响因子，比如：其他数据相同、落下场地相同时，葡萄比苹果弹得高
-    protected float frictionfactor;   //摩擦力的影响因子，影响球滑多远
-    protected int value;  //不同类型的球，得分可能不一样
-    private Sprite sp;
-    protected Rigidbody rgbd;
-    //protected BallMove bm;    
-    //public abstract void Move();
-    public float Quality
-    { get { return quality; } }
-    public float Jumpfactor
-    { get { return jumpfactor; } }
-    public float Frictionfactor
-    { get { return frictionfactor; } }
+    [SerializeField] protected float ballMass;  //球的质量
+    [SerializeField] protected string type;    //球的类型，例如："apple"、"orange"之类的
+    [SerializeField] protected float liftForceRatio=0.1f;   //球的升力
+    [SerializeField] protected float windRatio=1.0f;   //球的风力系数
+    [SerializeField] protected int value;  //不同类型的球，得分可能不一样
+    protected Vector3 moveSpeed = Vector3.zero;  //球运动的速度，初始值为0
+    private Sprite sp;   //球的贴图
+    public Rigidbody rgbd;
+    public float radius = 0f;  //球旋转的角度，弧度制
+    public float angle = 0f;   //球旋转的角度，角度制
+    public bool canStop = false;   //球停止运动
+
+    public float BallMass
+    { get { return ballMass; } }
+    public float LiftForceRatio
+    { get { return liftForceRatio; } }
+    public float WindRatio
+    { get { return windRatio; } }
     public int Value
     { get { return value; } }
     public string Type
     { get { return type; } }
+    public Vector3 MoveSpeed
+    {
+        set { moveSpeed = value; }
+    }
     public Sprite sprite
     {
         set { sp = value; }
     }
-    public Rigidbody Rgbd
-    {
-        set { rgbd = value; }
-        get { return rgbd; }
-    }
-    public abstract void SpecialEffect();    //球的特殊效果，例如：河豚会游泳
+
+    public virtual void SpecialMove()    //球的特殊移动，例如：河豚会游泳
+    {}
 }
