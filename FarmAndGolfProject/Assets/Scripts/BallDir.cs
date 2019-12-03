ac7bcc456d7bd1aa31a2b888a7bac154e1312cd8
jiang jie <2822569160@qq.com>
2019-12-03 18:41:56 +0800

    鼠标点击 键盘响应
    实现了鼠标点击选择终点，z键将球射击出去的基本模型

---
+++ FarmAndGolfProject/Assets/Scripts/BallDir.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallDir : MonoBehaviour
+{
+    #region private
+    /*射线检测*/
+    //private RaycastHit _hit;
+    private Vector3 pos;
+    #endregion
+    
+    #region public
+    /*鼠标点击方向*/
+    public Vector3 Pos
+    {
+        get => pos;
+        set => pos = value;
+    }
+    /*画线组件*/
+    public LineRenderer lineRenderer;
+    /*起点终点方向虚线所用的材质*/
+    public Material material;
+    /*要实例化的球*/
+    public GameObject ballObj;
+    /*起点终点方向虚线uv的移动速度*/
+    public float speed;
+    #endregion
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+//        if(lineRenderer != null)
+//            material = lineRenderer.gameObject    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offset = Time.time * speed;
+        material.mainTextureOffset = new Vector2(offset, 0);
+        
+        //点击
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit _hit;
+            bool isHit = Physics.Raycast(ray, out _hit);
+            if (isHit)
+            {
+                pos = _hit.point;
+                Debug.Log(pos);
+                lineRenderer.enabled = true;
+                //GameObject bal    l = Instantiate(ballObj, new Vector3(0, 0, -1.15f), Quaternion.identity);
+                lineRenderer.SetPosition(0, new Vector3(0, 0 , -1.15f));
+                lineRenderer.SetPosition(1, new Vector3(pos.x, pos.y,-1.15f));
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            lineRenderer.enabled = false;
+            GameObject ball = Instantiate(ballObj, new Vector3(0, 0, -1.15f), Quaternion.identity);
+        }
+    }
+}
--- FarmAndGolfProject/Assets/Scripts/BallMove.cs
+++ FarmAndGolfProject/Assets/Scripts/BallMove.cs
@@ -16,8 +16,12 @@ public class BallMove : MonoBehaviour
     /*横截面积*/
     [SerializeField]
     private float crossArea = 1.0f;
-    /*竖直方向速度归零的阈值*/
-    public const float speedThreshold = 50.0f;
+    /*Z方向速度归零的阈值*/
+    public float speedThresholdZ = 1.0f;
+    /*y方向速度归零的阈值*/
+    public float speedThresholdY = 0.2f;
+    /*x方向速度归零的阈值*/
+    public float speedThresholdX = 0.2f;
     #endregion
 
     /// Ball
@@ -38,6 +42,10 @@ public class BallMove : MonoBehaviour
     public Vector3 liftForce;
     /*风力系数(跟判定有关)*/
     public float windRatio = 1.0f;
+    /*三个方向上的初速度*/
+    public float movespeedX;
+    public float movespeedY;
+    public float movespeedZ;
 
     /// BallArm
     /*击打的方向和力*/
@@ -50,6 +58,7 @@ public class BallMove : MonoBehaviour
     public float groundBounceY;
 
     public float groundBounceX;
+    public float groundBounceZ;
     /*地面摩擦力*/
     public float groundFrictionY;
     public float groundFrictionX;
@@ -59,22 +68,27 @@ public class BallMove : MonoBehaviour
     /*风力(跟球有关)*/
     public Vector3 windDirection;
     
-    //
-    public LineRenderer lineRenderer;
+    
+    /// 画线相关
+    /*画线组件*/
+    private LineRenderer lineRenderer;
     //索引
     private int index;
     //点的数目
     private int dotCount;
-    //修正后的轨迹的点的位置
-    private Vector3 dotPos;
+//    //修正后的轨迹的点的位置
+//    private Vector3 dotPos;
     
     // Start is called before the first frame update
     void Start()
     {
-        moveSpeed.x = hitDirection.x;
-        moveSpeed.y = hitDirection.y;
-        moveSpeed.z = hitDirection.z;
-
+        hitDirection = (GameObject.FindWithTag("GameController").GetComponent<BallDir>().Pos - transform.position);
+        moveSpeed.x = hitDirection.x / movespeedX;
+        moveSpeed.y = hitDirection.y / movespeedY;
+        moveSpeed.z = movespeedZ;
+        
+        canStop = false;
+        lineRenderer = GameObject.FindWithTag("GameController").GetComponent<LineRenderer>();
         rb = this.GetComponent<Rigidbody>();
         dotCount = 0;
     }
@@ -86,9 +100,9 @@ public class BallMove : MonoBehaviour
         airForceDirection = 0.5f * AirDensity * shapeParameter * crossArea * moveSpeed.magnitude * (-moveSpeed);
         
         //升力
-        liftForce = Vector3.Cross(Vector3.right, moveSpeed).normalized;
-        if(liftForce.z != 0)
-            liftForce = new Vector3(0,liftForce.y * (liftForceRatio * moveSpeed.z) / liftForce.z, liftForceRatio * moveSpeed.z);
+        liftForce = Vector3.Cross(Vector3.right, moveSpeed.normalized).normalized;
+//        if(liftForce.z != 0)
+//            liftForce = new Vector3(0,liftForce.y * (liftForceRatio * moveSpeed.z) / liftForce.z, liftForceRatio * moveSpeed.z);
         
         //风力
         windDirection = ballMass * windRatio * windDirection.normalized;
@@ -98,8 +112,12 @@ public class BallMove : MonoBehaviour
             //速度
             moveSpeed.z = 0;
             //f/m=a
-            moveSpeed.y = (moveSpeed.y <= 0) ? 0 : moveSpeed.y - groundFrictionY / ballMass * Time.fixedDeltaTime;
-            moveSpeed.x = (moveSpeed.x <= 0) ? 0 : moveSpeed.x - groundFrictionX / ballMass * Time.fixedDeltaTime;
+            moveSpeed.y = (Mathf.Abs(moveSpeed.y) <= speedThresholdY) ? 0 : moveSpeed.y - 
+                                                                            moveSpeed.y / Mathf.Abs(moveSpeed.y)
+                                                                            * groundFrictionY / ballMass * Time.fixedDeltaTime;
+            moveSpeed.x = (Mathf.Abs(moveSpeed.x) <= speedThresholdX) ? 0 : moveSpeed.x - 
+                                                                            moveSpeed.x / Mathf.Abs(moveSpeed.x) 
+                                                                            * groundFrictionX / ballMass * Time.fixedDeltaTime;
             
         }
         if (!canStop)
@@ -111,15 +129,18 @@ public class BallMove : MonoBehaviour
         }
         transform.Translate(moveSpeed * Time.fixedDeltaTime,
             Space.World);
+        
+        ///绘制红色的轨迹
         //点数加一
         dotCount++;
-        dotPos += new Vector3(Mathf.Abs(transform.position.z - (-1)), transform.position.y,-1);
+        Vector3 dotPos = new Vector3(transform.position.x + Mathf.Abs(transform.position.z - (-1.15f)), transform.position.y,-1.15f);
+        lineRenderer.SetVertexCount(dotCount);
 
-//        while (index < dotCount)
-//        {
-//            lineRenderer.SetPosition(index, dotPos);
-//            index++;
-//        }
+        while (index < dotCount)
+        {
+            lineRenderer.SetPosition(index, dotPos);
+            index++;
+        }
 
         //角度
 //        radius = Mathf.Atan(moveSpeed.z / moveSpeed.y);
@@ -141,9 +162,10 @@ public class BallMove : MonoBehaviour
             Vector3 newDir = Vector3.zero;
             Vector3 curDir = moveSpeed;
             newDir = Vector3.Reflect(curDir, contactPoint.normal);
-            //竖直方向速度衰减，水平方向衰减
-            moveSpeed = new Vector3(newDir.x, groundBounceX * newDir.y, groundBounceX * newDir.z);
-            //moveSpeed = new Vector3(groundBounceX * newDir.x, newDir.y, newDir.z);
+            Debug.Log(newDir + "newDir");
+            //速度衰减
+            moveSpeed = new Vector3(groundBounceX* newDir.x, groundBounceY * newDir.y, groundBounceZ * newDir.z);
+            
             
             //升力系数置零
             liftForceRatio = 0;
@@ -152,7 +174,7 @@ public class BallMove : MonoBehaviour
             windRatio = 0;
 
             //竖直方向速度归零
-            if (moveSpeed.sqrMagnitude < speedThreshold)
+            if (moveSpeed.z < speedThresholdZ)
             {
                 canStop = true;
             }
@@ -160,18 +182,18 @@ public class BallMove : MonoBehaviour
     }
 
      
-    /// <summary>
-    /// 基础线绘制
-    /// </summary>
-    /// <param name="startPoint"></param>
-    /// <param name="endPoint"></param>
-    void DrawBaseLine(Vector2 startPoint,Vector2 endPoint)
-    {
-        //Graphics.DrawMesh();
-        //Debug.DrawLine(startPoint, endPoint, Color.black);
-        lineRenderer.SetPosition(0,new Vector3(startPoint.x,startPoint.y,-1));
-        lineRenderer.SetPosition(1,new Vector3(endPoint.x,endPoint.y,-1));
-    }
+//    /// <summary>
+//    /// 基础线绘制
+//    /// </summary>
+//    /// <param name="startPoint"></param>
+//    /// <param name="endPoint"></param>
+//    void DrawBaseLine(Vector2 startPoint,Vector2 endPoint)
+//    {
+//        //Graphics.DrawMesh();
+//        //Debug.DrawLine(startPoint, endPoint, Color.black);
+//        lineRenderer.SetPosition(0,new Vector3(startPoint.x,startPoint.y,-1));
+//        lineRenderer.SetPosition(1,new Vector3(endPoint.x,endPoint.y,-1));
+//    }
 
     void DrawParaCurveLine()
     {
