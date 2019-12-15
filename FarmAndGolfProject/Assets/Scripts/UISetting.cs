13d9859e6834cf610d4deb34b87e67f0b635f3a4
texwood0935 <1213712563@qq.com>
2019-12-15 14:57:06 +0800

    添加了本地保存数据，用PlayerFrebs实现的

--- FarmAndGolfProject/Assets/Scripts/UISetting.cs
+++ FarmAndGolfProject/Assets/Scripts/UISetting.cs
@@ -7,6 +7,7 @@ public class UISetting : MonoBehaviour
 {
     //单例模式
     private static UISetting _instance;
+    //让它在场景切换的时候不被摧毁
     public static UISetting Instance
     { get
         {
@@ -19,24 +20,23 @@ public class UISetting : MonoBehaviour
             return _instance;
         }
     }
-    //让它在场景切换的时候不被摧毁
     //按键设置
     public Dictionary<string, KeyCode> btn_code = new Dictionary<string, KeyCode>();
     //屏幕分辨率
-    [SerializeField] private bool isFullScreen = false;
+    [SerializeField] private bool isFullScreen = bool.Parse(PlayerPrefs.GetString("isFullScreen", "false"));
     //音量设置方面
-    [SerializeField] private float mainMusicValue = 1.0f;  //主音量的值
-    [SerializeField] private float bgMusicValue = 1.0f;    //背景音量的值
-    [SerializeField] private float effectMusicValue = 1.0f; //音效音量的值
+    [SerializeField] private float mainMusicValue = PlayerPrefs.GetFloat("mainMusicValue", 1.0f);  //主音量的值
+    [SerializeField] private float bgMusicValue = PlayerPrefs.GetFloat("bgMusicValue", 1.0f);    //背景音量的值
+    [SerializeField] private float effectMusicValue = PlayerPrefs.GetFloat("effectMusicValue", 1.0f); //音效音量的值
     public float MainMusicValue
-    { set { mainMusicValue = value;} get { return mainMusicValue; } }
+    { set { mainMusicValue = value;PlayerPrefs.SetFloat("mainMusicValue", mainMusicValue); } get { return mainMusicValue; } }
     public float BgMusicValue
-    { set { bgMusicValue = value; } get { return bgMusicValue; } }
+    { set { bgMusicValue = value; PlayerPrefs.SetFloat("bgMusicValue", bgMusicValue); } get { return bgMusicValue; } }
     public float EffectMusicValue
-    { set { effectMusicValue = value;} get { return effectMusicValue; } }
+    { set { effectMusicValue = value; PlayerPrefs.SetFloat("effectMusicValue", effectMusicValue); } get { return effectMusicValue; } }
     //屏幕分辨率设置
     public bool IsFullScreen
-    { set { isFullScreen = value; SetScreen(value); }get { return isFullScreen; } }
+    { set { isFullScreen = value; SetScreen(value); PlayerPrefs.SetString("isFullScreen", isFullScreen.ToString()); }get { return isFullScreen; } }
     private void SetScreen(bool value)  //如果不是全屏，则是1920×1080的分辨率
     {
         if (value)
