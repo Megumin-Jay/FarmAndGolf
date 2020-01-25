4556527d9ce60a5ad834fbb9aa5b9cd21fc8947b
Issac-v <1914999099@qq.com>
2020-01-25 22:52:46 +0800

    重装电脑的保险性提交
    不知道为啥，重装之前我提交的那个版本我看不到。。

---
+++ FarmAndGolfProject/Assets/Scripts/Active.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//用来切换panel
+public class Active : MonoBehaviour
+{
+    public GameObject NowPanel;
+    public GameObject NextPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void changer()
+    {
+        NowPanel.SetActive(false);
+        NextPanel.SetActive(true);
+    }
+}
