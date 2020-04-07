b3591082e6e6bbc40d1531aa31d3b6764c1cc03d
texwood0935 <1213712563@qq.com>
2020-04-07 16:15:31 +0800

    合并项目，上传牧场、农场场景，没有贴图

---
+++ FarmAndGolfProject/Assets/Scripts/Farm/FarmUI.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarmUI : MonoBehaviour
+{
+    public GameObject farmOp;
+    public FarmOperation fop;
+    public PastureOP pop;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        farmOp.SetActive(false);
+        if(fop==null)
+            fop = GameObject.FindGameObjectWithTag("Player").GetComponent<FarmOperation>();
+        if (pop == null)
+            pop = GameObject.FindGameObjectWithTag("Player").GetComponent<PastureOP>();
+    }
+
+    public void FarmOpUI()
+    {
+        farmOp.SetActive(!farmOp.activeSelf);
+        if (!farmOp.activeSelf)
+        {
+            if(fop!=null)
+                fop.SetReset();
+            if (pop != null)
+                pop.SetReset();
+        }
+    }
+}
---
+++ FarmAndGolfProject/Assets/Scripts/Farm/Select.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Select : MonoBehaviour
+{
+    [SerializeField] Camera cam;
+    public GameObject player;
+    [SerializeField] Vector3 mp;//鼠标位置
+    private float targetingRayLength = Mathf.Infinity;//射线的长度
+
+    void Start()
+    {
+        cam = this.GetComponent<Camera>();
+    }
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            TarRaycast();
+        }
+    }
+
+    public void TarRaycast()
+    {
+        mp = Input.mousePosition;
+        if (cam != null)
+        {
+            RaycastHit hitInfo;
+            Ray ray = cam.ScreenPointToRay(new Vector3(mp.x, mp.y, 0f));
+            if (Physics.Raycast(ray, out hitInfo, targetingRayLength))
+            {
+                if(hitInfo.transform.tag=="Earth")
+                {
+                    FarmOperation far = player.GetComponent<FarmOperation>();
+                    if(far!=null)
+                    {
+                        far.My_earth = hitInfo.transform.gameObject.GetComponent<Earth>();
+                    }
+                }
+                else if(hitInfo.transform.tag == "Animal")
+                {
+                    PastureOP pas = player.GetComponent<PastureOP>();
+                    if(pas!=null)
+                    {
+                        pas.My_Ani= hitInfo.transform.gameObject.GetComponent<AnimalGrowth>();
+                    }
+                }
+            }
+        }
+    }
+}
